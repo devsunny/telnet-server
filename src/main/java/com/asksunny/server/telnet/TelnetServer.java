4121973dbf0dcb8e3d4daeaed2056a93bdd82568
Developer Sunny <sunnyliu2@gmail.com>
2013-08-01 20:18:13 -0400

    fix bug

--- src/main/java/com/asksunny/server/telnet/TelnetServer.java
+++ src/main/java/com/asksunny/server/telnet/TelnetServer.java
@@ -2,6 +2,7 @@ package com.asksunny.server.telnet;
 import org.apache.commons.cli.CommandLine;
 import org.apache.commons.cli.HelpFormatter;
 import org.apache.commons.cli.Options;
+import org.slf4j.LoggerFactory;
 
 import com.asksunny.cli.utils.CLIOptionAnnotationBasedBinder;
 import com.asksunny.server.ServerConfig;
@@ -18,9 +19,12 @@ import io.netty.channel.socket.nio.NioServerSocketChannel;
 public class TelnetServer {
 
     private final ServerConfig config;
-
+    private static final org.slf4j.Logger logger = LoggerFactory.getLogger(TelnetServerHandler.class.getName());
+    public static final boolean isWIndows = System.getProperty("os.name").toLowerCase().indexOf("windows")!=-1;
+    
     public TelnetServer(ServerConfig config) {
         this.config = config;
+        logger.info("{} is starting on {} at port {}.", this.getClass().getName(), System.getProperty("os.name"), config.getPort());
     }
 
     public void run() throws Exception {
@@ -38,6 +42,7 @@ public class TelnetServer {
 		            workerGroup.shutdownGracefully();					
 				}
 			}));            
+            
             if(config.getBindingAddress()==null || config.getBindingAddress().isEmpty()){
             	b.bind(config.getPort()).sync().channel().closeFuture().sync();      
             }else{
@@ -54,14 +59,12 @@ public class TelnetServer {
        
     	ServerConfig config = new ServerConfig();
     	Options options = CLIOptionAnnotationBasedBinder.getOptions(config);    	
-    	CommandLine cli = CLIOptionAnnotationBasedBinder.bindPosix(options, args, config);
-    	
+    	CommandLine cli = CLIOptionAnnotationBasedBinder.bindPosix(options, args, config);    	
     	if(config.getPort()==0){
     		HelpFormatter formatter = new HelpFormatter();
     		formatter.printHelp("TelnetServer", options);
     		System.exit(1);
-    	}
-    	    	
+    	}    	    	
     	new TelnetServer(config).run();
     	
 //    	int port;        
--- src/main/java/com/asksunny/server/telnet/TelnetServerHandler.java
+++ src/main/java/com/asksunny/server/telnet/TelnetServerHandler.java
@@ -7,14 +7,12 @@ import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.SimpleChannelInboundHandler;
 
 import java.io.BufferedReader;
-import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.net.InetAddress;
 import java.util.ArrayList;
-import java.util.Date;
+import java.util.Arrays;
 import java.util.List;
-import java.util.logging.Level;
-import java.util.logging.Logger;
+
+import org.slf4j.LoggerFactory;
 
 import com.asksunny.cli.utils.RemoteCommand;
 import com.asksunny.cli.utils.RemoteCommandParser;
@@ -26,8 +24,12 @@ import com.asksunny.utils.OSUtil;
 @Sharable
 public class TelnetServerHandler extends SimpleChannelInboundHandler<String> {
 
-    private static final Logger logger = Logger.getLogger(TelnetServerHandler.class.getName());
+    private static final org.slf4j.Logger logger = LoggerFactory.getLogger(TelnetServerHandler.class.getName());
     public static final String EXIT = "exit";
+    public static final String BYE = "bye";
+    public static final String QUIT = "quit";
+    
+    public static final String SHUTDOWN = "shutdown";
     public static final String NEWLINE = OSUtil.OsLineSeparator();
     @Override
     public void channelActive(ChannelHandlerContext ctx) throws Exception {
@@ -47,37 +49,56 @@ public class TelnetServerHandler extends SimpleChannelInboundHandler<String> {
         
         ChannelFuture future = null;
         try{
-        
+        	if(logger.isDebugEnabled()) logger.debug("Recieved Request:{}", request);
         	RemoteCommand[] cmds = RemoteCommandParser.parseCommand(request);       
 	        for (RemoteCommand cmd : cmds) {
 				String[] cmdarray = cmd.getCmdArray();						 
-				if(cmdarray[0].equalsIgnoreCase(EXIT)){
+				if(cmdarray[0].equalsIgnoreCase(EXIT) || cmdarray[0].equalsIgnoreCase(BYE) || cmdarray[0].equalsIgnoreCase(QUIT) ){
 					if(future!=null){
 						future.addListener(ChannelFutureListener.CLOSE);
 					}else{
 						ctx.close();
 			        	return;
 					}
+				}else if(cmdarray[0].equalsIgnoreCase(SHUTDOWN)  ){
+					if(future!=null){
+						future.addListener(ChannelFutureListener.CLOSE);
+					}else{
+						ctx.close();			        	
+					}
+					Runtime.getRuntime().exit(0);	
 				}
-				Runtime rt = Runtime.getRuntime();
-				Process p = rt.exec(cmdarray);
+				ProcessBuilder builder = null;				
+				if(TelnetServer.isWIndows){
+					List<String> cmdss = new ArrayList<String>(Arrays.asList(cmdarray));
+					cmdss.add(0, "cmd.exe");
+					cmdss.add(1, "/C");
+					builder  = new ProcessBuilder(cmdss);
+					if(logger.isDebugEnabled()) logger.debug("Cmd processed:{}", cmdss);
+				}else{
+					List<String> cmdss = Arrays.asList(cmdarray);
+					builder  = new ProcessBuilder(cmdss);
+					if(logger.isDebugEnabled()) logger.debug("Cmd processed:{}", cmdss);
+				}
+				builder.environment().putAll(System.getenv());
+				builder.redirectErrorStream(true);
+				Process p = builder.start();				
 				BufferedReader br =new BufferedReader( new InputStreamReader(p.getInputStream() ));
 		        String line = null;
 		        while((line=br.readLine())!=null){
 		        	 future = ctx.write(line+NEWLINE);
 		        }
-		        br.close();        	
+		        br.close();		        
+		        p.waitFor();
 			}        
         }catch(Throwable ex){
         	ctx.write("[ERROR]" + ex.toString()+NEWLINE);
+        	logger.warn("Failed to process command.", ex);
         } 
     }
 
     @Override
-    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
-        logger.log(
-                Level.WARNING,
-                "Unexpected exception from downstream.", cause);
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {       
         ctx.close();
     }
 }
